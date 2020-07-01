66327afeb6a39cbbdf81ca8a19a5cc045e123dd5
Sergey Chekanskiy <chekanskiy@gmail.com>
2020-07-01 15:41:12 +0200

    fixed docker docs

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run -it -v ~/Documents/projects/:/projects --env-file .env schekan/great-expectations:latest /bin/bash -c "python /eval_scripts/eval_suite.py"
+COMMAND=$1 # "python /eval_scripts/eval_suite.py"
+docker run -it -v ~/Documents/projects/:/projects --env-file .env -p 9999:8888 schekan/great-expectations:latest /bin/bash -c "$COMMAND"
